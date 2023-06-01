db6ad20ab47f49242df01a62fa3f20295d545c01
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-06-01 15:28:20 -0500

    lab 11 branch

--- src/About.js
+++ src/About.js
@@ -4,8 +4,29 @@ class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    const developers = [
+      { name: "Carl Sanders", role: " Frontend Developer" },
+      { name: "Daron Faulkner", role: " Backend Developer" },
+    ];
+
+     return (
+      <div>
+        <h2>About the Developers</h2>
+        {developers.map((developer, index) => (
+          <div key={index}>
+            <h3>{developer.name}</h3>
+            <p>Role: {developer.role}</p>
+          </div>
+        ))}
+        <p>
+          This Page is about having a book search. You can upload your own books
+          or even your favorite books. It is designed to save your preference
+          style of books.
+        </p>
+      </div>
+    );
   }
-};
+}
+
 
 export default Profile;
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,31 +1,40 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
-  }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+function BestBooks() {
+  const [books, setBooks] = useState([]);
 
-  render() {
+  useEffect(() => {
+    // Make a GET request to your API to fetch all the books
+    fetch('http://localhost:3001/books')
+      .then(response => response.json())
+      .then(data => {
+        setBooks(data); // Update the books state with the fetched data
+      })
+      .catch(error => {
+        console.log('Error fetching books:', error);
+      });
+  }, []);
+  return <>
+    <h2>My Essential Lifelong Learning & amp; Formation Shelf</h2>
 
-    /* TODO: render all the books in a Carousel */
-
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    )
-  }
+    {books.length > 0 ? (
+      <Carousel>
+        {books.map(book => (
+          <div key={book._id}>
+            <h3>{book.title}</h3>
+            <p>{book.description}</p>
+            <p>Status: {book.status}</p>
+          </div>
+        ))}
+      </Carousel>
+    ) : (
+      <h3>No Books Found :</h3>
+    )}</>
 }
+
 
 export default BestBooks;
