ecdbba8af751531a2fa64fb3c6a444d27cccbdc7
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-06-05 15:17:11 -0500

    test

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 // import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 
 function BestBooks() {
   const [books, setBooks] = useState([]);
+  const [bookId, setbookId] = useState("");
 
   useEffect(() => {
     // Make a GET request to your API to fetch all the books
-    fetch('http://localhost:3001/books')
+    fetch('https://canofbooksbackend.onrender.com/books')
       .then(response => response.json())
       .then(data => {
         setBooks(data); // Update the books state with the fetched data
@@ -17,8 +19,56 @@ function BestBooks() {
       .catch(error => {
         console.log('Error fetching books:', error);
       });
-  }, []);
-  return <>
+    }, []);
+    const fetchBooks = () => {
+      // Make a GET request to fetch all the books
+      axios.get('https://canofbooksbackend.onrender.com/books/')
+        .then(response => {
+          setBooks(response.data);
+        })
+        .catch(error => {
+          console.error('Failed to fetch books:', error);
+        });
+    };
+    const deleteBook = (bookId) => {
+      // Make a DELETE request to delete a book by ID
+      console.log(bookId,"lol")
+      axios.delete(`https://canofbooksbackend.onrender.com/books/`+bookId)
+        .then(response => {
+          console.log('Book deleted successfully');
+          fetchBooks(); // Fetch updated book list after deletion
+        })
+        .catch(error => {
+          console.error('Failed to delete book:', error);
+        });
+    };
+  
+    return (
+      <div>
+        <h2>My Books</h2>
+        {books.length > 0 ? (
+          
+          <ul>
+            {books.map(book => (
+              <>
+              {console.log(book)}
+              <li key={book._id}>
+                  
+                <h3>{book.title}</h3>
+                <p>{book.description}</p>
+                <button onClick={() => {deleteBook(book._id)}}>Delete</button>
+              </li>
+              </>
+            ))}
+            
+          </ul>
+          
+        ) : (
+          <p>No books found. The book collection is empty.</p>
+        )}
+      </div>
+    );
+    return <>
     <h2>My Essential Lifelong Learning & amp; Formation Shelf</h2>
 
     {books.length > 0 ? (
@@ -34,7 +84,8 @@ function BestBooks() {
     ) : (
       <h3>No Books Found :</h3>
     )}</>
-}
+  }
+
 
 
 export default BestBooks;
